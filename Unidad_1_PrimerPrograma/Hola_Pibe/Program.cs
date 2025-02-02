1fd4da35a6ecb3380578e462341eb62cfb789e1a
unknown <camiloed_2000@hotmail.com>
2025-02-02 11:57:45 -0300

    ulalaa

---
+++ Unidad_1_PrimerPrograma/Hola_Pibe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hola_Pibe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Hola Mundo*/
+            Console.WriteLine("HOLA PIBE!!");
+            Console.ReadKey();
+        }
+    }
+}
